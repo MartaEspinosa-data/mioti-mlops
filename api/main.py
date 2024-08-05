3502880ad3b73dc66475a4b6d1c261c1cf0e9ee6
Marta Espinosa <martaespinosagarcia@gmail.com>
2024-08-06 01:47:13 +0200

    Challenge mlops Marta Espinosa

--- api/main.py
+++ api/main.py
@@ -47,13 +47,145 @@ Datos de entrada del modelo:
 }
 
 """
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 import joblib
 import pandas as pd
+from pydantic import BaseModel
 
 model = joblib.load('model.sav')
-
 app = FastAPI()
+
+#=========
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": False,
+    },
+    "a": {
+        "username": "Daniel",
+        "full_name": "a",
+        "email": "a@example.com",
+        "hashed_password": "fakehasheda",
+        "disabled": False,
+    },
+}
+
+
+
+def fake_hash_password(password: str):
+    return "fakehashed" + password
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def fake_decode_token(token):
+    # This doesn't provide any security at all
+    # Check the next version
+    user = get_user(fake_users_db, token)
+    return user
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+# ========================================================================
+
+
+class HeartAttackPredictionInput(BaseModel):
+    age: int
+    hypertension: int
+    gender: str
+    ever_married_Yes: int
+    heart_disease: int
+    avg_glucose_level: float
+    bmi: float
+    work_type: str
+    residence_type: str
+    smoking_status: str
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                "age": 33,
+                "hypertension": 1,
+                "gender": "male",
+                "ever_married_Yes": 1,
+                "heart_disease": 0,
+                "avg_glucose_level": 70.0,
+                "bmi": 29.0,
+                "work_type": "private",
+                "residence_type": "urban",
+                "smoking_status": "never smoked"
+            }
+            ]
+        }
+    }
+
+
+
+
+
 
 def gender_encoding(message):
     gender_encoded = {'gender_Male': 0, 'gender_Other': 0}
@@ -128,7 +260,7 @@ def main():
     return {'message': 'Hola'}
 
 @app.post('/heart-attack-prediction/')
-def predict_heart_attack(message: dict):
+def predict_heart_attack(message: dict, current_user: Annotated[User, Depends(get_current_active_user)]):
     model_pred = heart_prediction(message)
     # return {'prediction': model_pred}
     return model_pred
